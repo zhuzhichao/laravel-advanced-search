a477eda1b19366109111e337d86df08d094bea39
zhuzhichao <me@zhuzhichao.com>
2020-11-12 14:45:02 +0800

    Update unit testing model

--- tests/Utils/Models/User.php
+++ tests/Utils/Models/User.php
@@ -3,7 +3,6 @@
 namespace Tests\Utils\Models;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -23,7 +22,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use SoftDeletes;
-    use HasFactory;
 
     /**
      * @var mixed[]
